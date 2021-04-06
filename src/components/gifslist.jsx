946793c749d925ef4e78cbd80119c3dc00f6ffbc
Tom Renard <tom.renard@zoho.com>
2021-04-06 13:27:59 +0200

    exo correction

--- src/components/gifslist.jsx
+++ src/components/gifslist.jsx
@@ -1,1 +1,12 @@
 import React, { Component } from 'react';
+import Gif from './gif';
+
+const GifList = (props) => {
+  return (
+    <div className="gif-list">
+      {props.gifs.map(gif => <Gif id={gif} key={gif} selectGif={props.selectGif} />)}
+    </div>
+  );
+};
+
+export default GifList;
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
-// eslint-disable-next-line import/extensions
 import App from './components/app.jsx';
 
 const root = document.getElementById('root');
